6b1fe01e6dde0f6f856d471aef80f8f00d077529
jshigu <info@yskeo.com>
2024-03-25 16:18:58 +0900

    Initial Commit

---
+++ gpl02 Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gpl02 Watch App
+//
+//  Created by 世吉 on 2024/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gpl02 Watch App/gpl02App.swift
@@ -0,0 +1,17 @@
+//
+//  gpl02App.swift
+//  gpl02 Watch App
+//
+//  Created by 世吉 on 2024/03/25.
+//
+
+import SwiftUI
+
+@main
+struct gpl02_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
